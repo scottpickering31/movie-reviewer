b861b8d4e77ce7c704fb176fcda536e1cbf1e476
Scott Pickering <scottpickering31@gmail.com>
2024-01-15 20:31:16 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+function App() {
+  return (
+    <div className="hello-scss">
+      <h1>Hello</h1>
+      <p className="hello-scss-child">World</p>
+    </div>
+  );
+}
+
+export default App;
